3f50d64ccc5a8e20396ef03f0b0d09efdcd2494e
Alvaro <chava_1117@hotmail.com>
2018-12-11 14:07:37 +0100

    Tercer commit, en el que se añade el área del rectángulo y algo de documentación del programa

--- CalculoDeAreas.java
+++ CalculoDeAreas.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class CalculoDeAreas {
 
+	
+	//Método que imprime en pantalla el menú con las diferentes opciones a seguir
 	public static void menu() {
 		System.out.println("CÁLCULO DE ÁREAS");
 		System.out.println("=====================");
@@ -13,14 +15,22 @@ public class CalculoDeAreas {
 		System.out.println("4. Salir");
 	}
 	
+	//Método que calcula el área del triángulo dados unos valores de base y altura
 	public static double calcularAreaTriangulo(int base, int altura) {
-		return base * altura;
+		return (base * altura)/2;
 	}
 	
+	//Método que calcula el área del trapecio dados unos valores de altura y dos lados.
 	public static double calcularAreaTrapecio(int altura, int lado1, int lado2) {
 		return ((lado1+lado2)/2)*altura;
 	}
 	
+	//Método que calcula el área del rectángulo dados unos valores de base y altura
+	public static double calcularAreaRectangulo(int base, int altura) {
+		return base * altura;
+	}
+	
+	//Main que llama a los anteriores métodos dependiendo de un parámetro numérico que se especifica en el menú previamente creado
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		menu();
@@ -43,6 +53,16 @@ public class CalculoDeAreas {
 			int lado2 = sc2.nextInt();
 			sc2.close();
 			System.out.println("El área del trapecio es: " + calcularAreaTrapecio(altura, lado1, lado2));
+		} else if (numeroMenu == 3) {
+			Scanner sc3 = new Scanner(System.in);
+			System.out.println("Inserte la altura");
+			int altura = sc3.nextInt();
+			System.out.println("Inserte la base");
+			int base = sc3.nextInt();
+			sc3.close();
+			System.out.println("El área del rectángulo es: " + calcularAreaRectangulo(altura, base));
+		} else {
+			System.exit(1);
 		}
 		sc.close();
 	}
